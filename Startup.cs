96e8725f59d5427c9306ca030f7f4b9c3f7d87ca
Kaue Reinbold <kaue.reinbold@outlook.com>
2020-06-14 20:13:14 -0300

    Register HttpClient service to better handle http calls.

--- Startup.cs
+++ Startup.cs
@@ -1,4 +1,7 @@
+using System;
 using LocationFunction;
+using LocationFunction.Interfaces;
+using LocationFunction.Services;
 using Microsoft.Azure.Functions.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -10,6 +13,8 @@ namespace LocationFunction
     {
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddHttpClient<IIBGELocationService, IBGELocationService>(options =>
+                options.BaseAddress = new Uri(Environment.GetEnvironmentVariable("IBGEBaseUrl")));
         }
 
         public override void Configure(IFunctionsHostBuilder builder) =>
